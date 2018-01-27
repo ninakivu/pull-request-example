a4bb80f07c4ec0018648d141b7093ae9bf60eb18
Dylan Knight <samuraiknight713@gmail.com>
2018-01-26 16:20:23 -0800

    second commit

---
+++ server.js
@@ -0,0 +1,60 @@
+var $tweetContainer = $('#tweet-container')
+var $ping = $('#ping')
+var socket = io()
+
+socket.on('connect', function() {
+    console.log("connected  ")
+})  
+
+$ping.on('click', function(){
+    socket.emit('ping message')
+})
+
+socket.on('pong message', function(){
+    alert("the server just ponged!!");
+})
+
+socket.on('incoming tweet', function(data) {
+    console.log('data')
+    $tweetContainer.preappend('<p>' + data.text + '</p>')
+})
+var $tweetContainer = $('#tweet-container')
+var $ping = $('#ping')
+var socket = io()
+
+socket.on('connect', function() {
+    console.log("connected  ")
+})  
+
+$ping.on('click', function(){
+    socket.emit('ping message')
+})
+
+socket.on('pong message', function(){
+    alert("the server just ponged!!");
+})
+
+socket.on('incoming tweet', function(data) {
+    console.log('data')
+    $tweetContainer.preappend('<p>' + data.text + '</p>')
+})
+var $tweetContainer = $('#tweet-container')
+var $ping = $('#ping')
+var socket = io()
+
+socket.on('connect', function() {
+    console.log("connected  ")
+})  
+
+$ping.on('click', function(){
+    socket.emit('ping message')
+})
+
+socket.on('pong message', function(){
+    alert("the server just ponged!!");
+})
+
+socket.on('incoming tweet', function(data) {
+    console.log('data')
+    $tweetContainer.preappend('<p>' + data.text + '</p>')
+})
